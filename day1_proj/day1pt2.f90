6d576f7dace6266a0faafce0b1b34b66d9ddcd4c
Don-Andes <don.andes@ibm.com>
2018-12-03 12:23:35 -0500

    Complete day1pt2

---
+++ day1_proj/day1pt2.f90
@@ -0,0 +1,52 @@
+program day1pt2
+    use dynamicArray
+
+    implicit none
+
+    ! Values
+    character(len=1) :: sign
+    character(len=200) :: line
+    character(len=199) :: prenum
+    integer :: number, ios, fileSize, frequency = 0
+    integer, dimension(:), allocatable :: previousFreqs
+
+
+    ! Open file as identifier 1
+    open(1, file = 'day1_input.txt', iostat=ios, status = 'old') 
+    if ( ios /= 0 ) stop "Error opening file"
+
+    allocate(previousFreqs(1))
+
+    ! Loop through values line by line
+    do
+        ! If we hit the EOF, loop back to top and keep going
+        read(1, *,iostat=ios) line
+        if (ios /= 0) then 
+            rewind(1)
+            cycle
+        end if
+
+        ! Break sign off of front of line
+        sign = trim(line(1:1))
+
+        ! Get number payload and convert to integer
+        prenum = trim(line(2:199))
+        read(prenum, *) number
+
+        if (sign == '+') then
+            frequency = frequency + number
+        else
+            frequency = frequency - number
+        end if
+
+        if (any(previousFreqs == frequency)) then
+            exit
+        end if
+
+        call addElementToArray(frequency, previousFreqs)
+    end do
+
+    write(*,*) frequency
+    deallocate(previousFreqs)
+
+end program day1pt2
---
+++ day1_proj/dynamicArray.f90
@@ -0,0 +1,31 @@
+module dynamicArray
+    contains
+
+    ! Takes an element and array, checks if array is already allocated
+    !  and if not allocates. If it is, creates new array at size + 1, 
+    !  copies old array to it, and moves allocation to the new array.
+    subroutine addElementToArray(element, array)
+        implicit none
+
+        integer :: i, isize
+        integer :: element
+        integer, dimension(:), allocatable :: array
+        integer, dimension(:), allocatable :: newArray
+
+        if (allocated(array)) then
+            isize = size(array)
+            allocate(newArray(isize+1))
+            do i=1, isize
+                newArray(i) = array(i)
+            end do
+            newArray(isize+1) = element
+            deallocate(array)
+
+            call move_alloc(newArray, array)
+
+        else
+            allocate(array(1))
+            array(1) = element
+        end if
+    end subroutine addElementToArray
+end module dynamicArray
